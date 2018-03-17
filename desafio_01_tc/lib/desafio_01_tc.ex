e94fb76c95dabece47bf932e305fcdd316c11c78
Auralcat <ashleyzt@hi2.in>
2018-03-17 19:37:20 -0300

    Finish challenge

--- desafio_01_tc/lib/desafio_01_tc.ex
+++ desafio_01_tc/lib/desafio_01_tc.ex
@@ -2,19 +2,25 @@ defmodule Desafio_01Tc do
   def mostrar_dica do
     # Escolhe um string aleatório de dica para mostrar
     dicas = ["Poção mágica", "líquido encantado em frasco"]
-    Random.choice(dicas)
+    "Dica: " <> Random.choice(dicas)
   end
 
   def adivinhar_palavra() do
     # Receber a entrada
     tentativa = IO.gets("Digite a palavra >> ") |> String.replace(~r/\r|\n/, "")
-    if (tentativa == "elixir") do
+    if (String.downcase(tentativa) == "elixir") do
       IO.puts("Parabéns, você acertou!")
     else
       IO.puts("Errado, você perdeu.")
     end
   end
+
+  def main do
+    # Apresentando o programa
+    IO.puts("Olá, prepare-se para adivinhar a palavra!")
+    IO.puts(mostrar_dica())
+    adivinhar_palavra()
+  end
 end
 
-# Apresentando o programa
-IO.puts("Olá, prepare-se para adivinhar a palavra!")
+Desafio_01Tc.main
