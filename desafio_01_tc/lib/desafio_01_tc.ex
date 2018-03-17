2752321121bef820add11f49f182a2e44d3268a5
Auralcat <ashleyzt@hi2.in>
2018-03-17 19:31:14 -0300

    Update Elixir, compile deps

--- desafio_01_tc/lib/desafio_01_tc.ex
+++ desafio_01_tc/lib/desafio_01_tc.ex
@@ -2,6 +2,17 @@ defmodule Desafio_01Tc do
   def mostrar_dica do
     # Escolhe um string aleatório de dica para mostrar
     dicas = ["Poção mágica", "líquido encantado em frasco"]
+    Random.choice(dicas)
+  end
+
+  def adivinhar_palavra() do
+    # Receber a entrada
+    tentativa = IO.gets("Digite a palavra >> ") |> String.replace(~r/\r|\n/, "")
+    if (tentativa == "elixir") do
+      IO.puts("Parabéns, você acertou!")
+    else
+      IO.puts("Errado, você perdeu.")
+    end
   end
 end
 
